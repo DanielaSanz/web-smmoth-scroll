4a7e88bc65672cf13fb89d1e6364f43f21a812b7
Daniela Sanchez <47358877+reinamarla@users.noreply.github.com>
2021-05-06 17:09:42 -0300

    se agrega footer

---
+++ react-website-smooth-scroll/src/components/Footer/index.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { 
+    FaInstagram,
+    FaFacebook,
+    FaTwitter,
+    FaYoutube, 
+    FaLinkedin 
+} from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcon,
+    SocialIconLink,
+} from './FooterElements';
+
+//href: poner url de social media
+const Footer = () => {
+    return (
+        <>
+            <FooterContainer>
+                <FooterWrap>
+                    <FooterLinksContainer>
+                        <FooterLinksWrapper>
+                            <FooterLinkItems>
+                                <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to='/'>How it works</FooterLink>
+                                <FooterLink to='/'>Testimonials</FooterLink>
+                                <FooterLink to='/'>Careers</FooterLink>
+                                <FooterLink to='/'>Investors</FooterLink>
+                                <FooterLink to='/'>Terms of Service</FooterLink> 
+                            </FooterLinkItems>
+                            <FooterLinkItems>
+                                <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to='/'>Contact</FooterLink>
+                                <FooterLink to='/'>Support</FooterLink>
+                                <FooterLink to='/'>Destinations</FooterLink>
+                                <FooterLink to='/'>Sponsorships</FooterLink>
+                            </FooterLinkItems>
+                        </FooterLinksWrapper>
+                        <FooterLinksWrapper>
+                            <FooterLinkItems>
+                                <FooterLinkTitle>Videos</FooterLinkTitle>
+                                <FooterLink to='/'>Submit Video</FooterLink>
+                                <FooterLink to='/'>Ambassadors</FooterLink>
+                                <FooterLink to='/'>Agency</FooterLink>
+                                <FooterLink to='/'>Influercer</FooterLink>
+                            </FooterLinkItems>
+                            <FooterLinkItems>
+                                <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to='/'>Instagram</FooterLink>
+                                <FooterLink to='/'>Facebook</FooterLink>
+                                <FooterLink to='/'>Youtube</FooterLink>
+                                <FooterLink to='/'>Twitter</FooterLink>
+                                <FooterLink to='/'>LinkedIn</FooterLink>
+                            </FooterLinkItems>
+                        </FooterLinksWrapper>
+                    </FooterLinksContainer>
+                    <SocialMedia>
+                        <SocialMediaWrap>
+                            <SocialLogo to='/'>Marla</SocialLogo>
+                            <WebsiteRights>
+                                Marla © {new Date().getFullYear()} All rights reserved.
+                            </WebsiteRights>
+                            <SocialIcon>
+                                <SocialIconLink 
+                                    href='/' 
+                                    target='_blank'
+                                    arial-label='Instagram'
+                                >
+                                    <FaInstagram/>
+                                </SocialIconLink>
+                                <SocialIconLink 
+                                    href='/' 
+                                    target='_blank'
+                                    arial-label='Facebook'
+                                >
+                                    <FaFacebook/>
+                                </SocialIconLink>
+                                <SocialIconLink 
+                                    href='/' 
+                                    target='_blank'
+                                    arial-label='Twitter'
+                                >
+                                    <FaTwitter/>
+                                </SocialIconLink>
+                                <SocialIconLink 
+                                    href='/' 
+                                    target='_blank'
+                                    arial-label='Youtube'
+                                >
+                                    <FaYoutube/>
+                                </SocialIconLink>
+                                <SocialIconLink 
+                                    href='/' 
+                                    target='_blank'
+                                    arial-label='Linkedin'
+                                >
+                                    <FaLinkedin/>
+                                </SocialIconLink>
+                            </SocialIcon>
+                        </SocialMediaWrap>
+                    </SocialMedia>
+                </FooterWrap>
+            </FooterContainer>
+        </>
+    )
+}
+
+export default Footer;
--- react-website-smooth-scroll/src/pages/index.js
+++ react-website-smooth-scroll/src/pages/index.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
-import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
 import Services from '../components/Services';
+import InfoSection from '../components/InfoSection';
+import Footer from '../components/Footer';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -22,6 +23,7 @@ const Home = () => {
             <InfoSection {...homeObjTwo}/>
             <Services/>
             <InfoSection {...homeObjThree}/>
+            <Footer/>
         </div>
     )
 }
